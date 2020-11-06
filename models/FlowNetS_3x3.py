2f9a8ef643e30de5dba5eb1cb0c852fb7dae7632
kairx772 <kaoweitse@lolab-nthu.org>
2020-11-06 23:44:25 +0800

    change stride order

--- models/FlowNetS_3x3.py
+++ models/FlowNetS_3x3.py
@@ -17,13 +17,13 @@ class FlowNetS(nn.Module):
         super(FlowNetS,self).__init__()
 
         self.batchNorm = batchNorm
-        self.conv1   = conv(self.batchNorm,   6,   64, stride=2) # 7x7 origin
+        self.conv1   = conv(self.batchNorm,   6,   64) # 7x7 origin
         self.conv1_1 = conv(self.batchNorm,  64,   64)
-        self.conv1_2 = conv(self.batchNorm,  64,   64)
-        self.conv2   = conv(self.batchNorm,  64,  128, stride=2) # 5x5 origin
-        self.conv2_1 = conv(self.batchNorm, 128,  128)
-        self.conv3   = conv(self.batchNorm, 128,  256, stride=2) # 5x5 origin
-        self.conv3_0 = conv(self.batchNorm, 256,  256)
+        self.conv1_2 = conv(self.batchNorm,  64,   64, stride=2)
+        self.conv2   = conv(self.batchNorm,  64,  128) # 5x5 origin
+        self.conv2_1 = conv(self.batchNorm, 128,  128, stride=2)
+        self.conv3   = conv(self.batchNorm, 128,  256) # 5x5 origin
+        self.conv3_0 = conv(self.batchNorm, 256,  256, stride=2)
         self.conv3_1 = conv(self.batchNorm, 256,  256)
         self.conv4   = conv(self.batchNorm, 256,  512, stride=2)
         self.conv4_1 = conv(self.batchNorm, 512,  512)
